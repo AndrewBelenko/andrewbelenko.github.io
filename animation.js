ad29bcbc656bb24e5e59c0769423533e422655de
AndrewBelenko <60070433+AndrewBelenko@users.noreply.github.com>
2020-04-17 00:54:51 +0300

    Add files via upload
    my first ever commit

---
+++ animation.js
@@ -0,0 +1,47 @@
+var mybutton = document.getElementById("myBtn");
+var colourCanged = false;
+var myTitle = document.getElementById("mytitle");  
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+
+function mouseDown() {
+    mybutton.textContent = "Mouse is down";    
+}
+
+
+function mouseUp() {
+    mybutton.textContent = "Go to top ↑";
+}
+
+function changeColour() {
+    if (!colourCanged) {
+        mybutton.style.background='#000000';
+    } else {
+        mybutton.style.background='#ff0000';
+    }
+
+    colourCanged = !colourCanged
+}
+
+
+function gotToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    myTitle.textContent = myTitle.textContent + "+";
+    
+}
